adb40321ce93d834e70f5b75a46899d6e8240e18
Decentnamed <dawid.grabowicz20@gmail.com>
2022-06-24 13:17:17 +0200

    Zadanie3

---
+++ Album.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public class Album : INotifyPropertyChanged
+    {
+
+        static private readonly Dictionary<string, string[]> powiązaneWłaściwości
+            = new Dictionary<string, string[]>()
+            {
+                ["Tytuł"] = new string[] { "Format"/*, "Format"*/},
+                ["Autor"] = new string[] { "Format"/*, "Format"*/},
+                ["Wydawca"] = new string[] { "Format"/*, "Format"*/},
+                ["Nośnik"] = new string[] { "Format"/*, "Format"*/},
+                ["DataWydania"] = new string[] { "Format" },
+            };
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(
+            [CallerMemberName] string właściwość = null,
+            HashSet<string> załatwioneWłaściwości = null
+            )
+        {
+            if (załatwioneWłaściwości == null)
+                załatwioneWłaściwości = new HashSet<string>();
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(właściwość));
+            załatwioneWłaściwości.Add(właściwość);
+            if (powiązaneWłaściwości.ContainsKey(właściwość))
+                foreach (string powiązanaWłaściwość in powiązaneWłaściwości[właściwość])
+                    if (!załatwioneWłaściwości.Contains(powiązanaWłaściwość))
+                        OnPropertyChanged(powiązanaWłaściwość, załatwioneWłaściwości);
+        }
+
+        string tytuł;
+        string autor;
+        string wydawca;
+        string nośnik;
+        DateTime? dataWydania;
+
+        public string Tytuł {
+            get => tytuł;
+            set
+            {
+                tytuł = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Autor
+        {
+            get => autor;
+            set
+            {
+                autor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Wydawca
+        {
+            get => wydawca;
+            set
+            {
+                wydawca = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Nośnik
+        {
+            get => nośnik;
+            set
+            {
+                nośnik = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime? DataWydania
+        {
+            get => dataWydania;
+            set
+            {
+                dataWydania = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Tytuł: {Tytuł}, Autor: {Autor}";
+        }
+
+        public string Format
+        {
+            get => $"Tytuł: {Tytuł}, Autor: {Autor}";
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    class Model
+    {
+        public ObservableCollection<Album> ListaAlbumów { get; } = new ObservableCollection<Album>() {
+            new Album() {Tytuł="Kompot", Autor="Paluch", Wydawca="BOR", Nośnik="Płyta CD", DataWydania=Convert.ToDateTime("2022.04.01")},
+            new Album() {Tytuł="Fight Club", Autor="PRO8L3M", Wydawca="PRO8L3M", Nośnik="Płyta CD", DataWydania=Convert.ToDateTime("2021.03.19")},
+            new Album() {Tytuł="Ghetto Playboy", Autor="Smolasty", Wydawca="Smolasty", Nośnik="Płyta CD", DataWydania=Convert.ToDateTime("2021.05.09")},
+            new Album() {Tytuł="Chaos", Autor="Fukaj", Wydawca="Fukaj", Nośnik="Płyta CD", DataWydania=Convert.ToDateTime("2021.01.29")},
+            new Album() {Tytuł="Ziomalski mixtape", Autor="Żabson", Wydawca="INTERNAZIOMALE", Nośnik="Płyta CD", DataWydania=Convert.ToDateTime("2021.05.13")},
+        };
+
+        internal Album Nowy()
+        {
+            Album nowy = new Album();
+            ListaAlbumów.Add(nowy);
+            return nowy;
+        }
+
+    }
+}
